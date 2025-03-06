bed2e1dc896ba89171fb1cef808bebfa21a009ed
Lucas Scognamiglio <lucas.scognamiglio@tero.us>
2025-03-06 13:07:03 -0300

    Added area to species detail

--- src/components/SpeciesDetail/SpeciesDetail.jsx
+++ src/components/SpeciesDetail/SpeciesDetail.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Comments from '../Comments/Comments';
 
 const SpeciesDetail = (props) => {
-
     const [modalShow, setModalShow] = useState(true);
+    const [areaName, setAreaName] = useState('');
     let SpeciesData = props.SpeciesTypeDetail;
     const user = useSelector((state) => state.user);
 
+    useEffect(() => {
+        const fetchAreas = async () => {
+            try {
+                const response = await fetch(
+                    `https://mammal-excited-tarpon.ngrok-free.app/api/natural-area/list?page=1&pageSize=1000`,
+                    {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'ngrok-skip-browser-warning': 'true'
+                        }
+                    }
+                );
+                const data = await response.json();
+                const matchingArea = data.items.find(area => area.id === SpeciesData.naturalAreaId);
+                if (matchingArea) {
+                    setAreaName(matchingArea.name);
+                }
+            } catch (err) {
+                setAreaName('Desconocido');
+            }
+        };
+        fetchAreas();
+    }, [SpeciesData.naturalAreaId]);
 
     function SpeciesDetailModal(props) {
         return (
@@ -31,6 +54,7 @@ const SpeciesDetail = (props) => {
                             <p>Nombre científico: {SpeciesData.scientificName}.</p>
                             <p>Categoría de especie: {SpeciesData.category}.</p>
                             <p>Estado de conservación: {SpeciesData.conservationStatus}</p>
+                            <p>Área natural: {areaName}</p>
                         </div>
                         {
                             user ?
@@ -49,15 +73,14 @@ const SpeciesDetail = (props) => {
         );
     }
 
-
     return (
         <>
             <SpeciesDetailModal
                 show={modalShow}
                 onHide={() => setModalShow(false)}
             />
-
-        </>)
+        </>
+    );
 }
 
-export default SpeciesDetail
+export default SpeciesDetail;
